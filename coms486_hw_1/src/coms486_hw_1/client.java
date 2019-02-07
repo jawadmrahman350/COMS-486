471a7c8fbf9f0023158ab44be68aa98810754112
jawadmrahman350 <jawad.mashrur@gmail.com>
2019-02-07 14:24:10 -0600

    initial commit

--- coms486_hw_1/src/coms486_hw_1/client.java
+++ coms486_hw_1/src/coms486_hw_1/client.java
@@ -1,5 +1,26 @@
 package coms486_hw_1;
-
+import java.io.*;  
+import java.net.*;  
 public class client {
 
+		  public static void main(String argv[]) throws Exception  
+		    {  
+		      String clientSentence;  
+		      String capitalizedSentence;  
+		      ServerSocket welcomeSocket = new ServerSocket(6789);  
+		      while(true) {  
+		            Socket connectionSocket = welcomeSocket.accept();  
+		           BufferedReader inFromClient =  
+		              new BufferedReader(new 
+		              InputStreamReader(connectionSocket.getInputStream()));  
+		           
+		           DataOutputStream  outToClient =  
+		                   new DataOutputStream(connectionSocket.getOutputStream());
+		                 clientSentence = inFromClient.readLine();  
+		                 capitalizedSentence = clientSentence.toUpperCase() + '\n';  
+		                 outToClient.writeBytes(capitalizedSentence);
+		      }
+		      
+		    }
+		  	
 }
--- coms486_hw_1/src/coms486_hw_1/server.java
+++ coms486_hw_1/src/coms486_hw_1/server.java
@@ -1,5 +1,25 @@
 package coms486_hw_1;
-
+import java.io.*;  
+import java.net.*;  
 public class server {
 
+    public static void main(String argv[]) throws Exception  
+    {  
+        String sentence;  
+        String modifiedSentence;  
+        BufferedReader inFromUser =  
+          new BufferedReader(new InputStreamReader(System.in));  
+        Socket clientSocket = new Socket("hostname", 6789);  
+        DataOutputStream outToServer =  
+          new DataOutputStream(clientSocket.getOutputStream());
+        
+        BufferedReader inFromServer =  
+                new BufferedReader(new 
+                InputStreamReader(clientSocket.getInputStream()));  
+              sentence = inFromUser.readLine();  
+              outToServer.writeBytes(sentence + '\n');  
+              modifiedSentence = inFromServer.readLine();  
+              System.out.println ("FROM SERVER: " + modifiedSentence);  
+              clientSocket.close();  
+    }
 }
