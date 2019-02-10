ba51437590aa349577ab60949b1a64faadd170e0
Jawad Mashrur Rahman <jawad.mashrur@gmail.com>
2019-02-10 15:21:25 -0600

    Update server.java

--- coms486_hw_1/src/coms486_hw_1/server.java
+++ coms486_hw_1/src/coms486_hw_1/server.java
@@ -1,25 +1,47 @@
-package coms486_hw_1;
+/** @author Jawad M Rahman **/
+
 import java.io.*;  
 import java.net.*;  
-public class server {
+public class Server {
 
-    public static void main(String argv[]) throws Exception  
-    {  
-        String sentence;  
-        String modifiedSentence;  
-        BufferedReader userIn =  
-          new BufferedReader(new InputStreamReader(System.in));  
-        Socket clientSocket = new Socket("hostname", 6789);  
-        DataOutputStream outToServer =  
-          new DataOutputStream(clientSocket.getOutputStream());
-        
-        BufferedReader inFromServer =  
-                new BufferedReader(new 
-                InputStreamReader(clientSocket.getInputStream()));  
-              sentence = userIn.readLine();  
-              outToServer.writeBytes(sentence + '\n');  
-              modifiedSentence = inFromServer.readLine();  
-              System.out.println ("FROM SERVER: " + modifiedSentence);  
-              clientSocket.close();  
-    }
+	public static void main(String argv[]) throws IOException  
+	{  
+		String sentence;
+		int port = 0;
+		String len;
+		if (argv.length == 1) 
+		{  //getting the port number 
+			port = Integer.parseInt(argv[0]);
+		} 
+
+		else{
+			if(argv.length == 0) {
+				System.out.println("No command line arguments found, please try again."); 
+			}
+			else {
+				System.out.println("Please enter the name of the program and port number and try again. ");
+
+			}
+			return;
+		}
+		//making the socket 
+		ServerSocket socketSvr = new ServerSocket(port);
+
+
+		while(true) {
+			
+			//Creating the socket which will manage the contact with client
+			Socket connectionSocket = socketSvr.accept();
+			//Attaching the input stream to the socket
+			BufferedReader inFromClient =new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			//Attaching the output stream from client
+			DataOutputStream outToClient =new DataOutputStream (connectionSocket.getOutputStream());
+			//get the length of the sentence from client and send the length
+			len = Integer.toString((inFromClient.readLine().length()));
+			System.out.println("Got the input and now sending the length");
+			outToClient.writeBytes(len);
+
+		}
+
+	}
 }
