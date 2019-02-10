36f8761b7a0c4bb2a555d7837d456435b3916163
Jawad Mashrur Rahman <jawad.mashrur@gmail.com>
2019-02-10 15:20:39 -0600

    Update client.java

--- coms486_hw_1/src/coms486_hw_1/client.java
+++ coms486_hw_1/src/coms486_hw_1/client.java
@@ -1,44 +1,57 @@
-package coms486_hw_1;
+
+/** @author Jawad M Rahman **/
+
 import java.io.*;  
 import java.net.*;  
+import java.util.Scanner;
 
-public class client {
+public class Client {
 
-		  public static void main(String argv[]) throws Exception  
-		    {  
-		      String clientSentence;  
-		      String capitalizedSentence;  
-		      String hostName;
-		      int port = 0; 
-		   // check if length of args array is 
-		        // greater than 0 
-		        if (argv.length > 0) 
-		        {  //gettign the hostname and the port number 
-		           hostName = argv[1];
-		           port = Integer.parseInt(argv[2]);
-//		            for (String val:argv) 
-//		                System.out.println(val); 
-		        } 
-		        else{
-		            System.out.println("No command line "+ 
-		                               "arguments found, please try again."); 
-		            return;
-		        }
-		        
-		      ServerSocket welcomeSocket = new ServerSocket(port);  
-		      while(true) {  
-		            Socket connectionSocket = welcomeSocket.accept();  
-		           BufferedReader inFromClient =  
-		              new BufferedReader(new 
-		              InputStreamReader(connectionSocket.getInputStream()));  
-		           
-		           DataOutputStream  outToClient =  
-		                   new DataOutputStream(connectionSocket.getOutputStream());
-		                 clientSentence = inFromClient.readLine();  
-		                 capitalizedSentence = clientSentence.toUpperCase() + '\n';  
-		                 outToClient.writeBytes(capitalizedSentence);
-		      }
-		      
-		    }
-		  	
+	public static void main(String argv[]) throws IOException  
+	{  	String serverSen;
+		String hostName;
+		int port = 0; 
+		// check if length of args array is 
+		// greater than 0 
+		if (argv.length == 2) 
+		{  //getting the hostname and the port number 
+			hostName = argv[0];
+			port = Integer.parseInt(argv[1]);
+		} 
+		else{
+			if(argv.length == 0) {
+				System.out.println("No command line arguments found, please try again."); 
+			}
+			else {
+				System.out.println("Please enter the name of the program, hostname and port number and try again.");
+			}
+			return;
+		}
+
+		Socket sock = new Socket(hostName, port);
+		while(true) {  
+			//Attaching output stream to socket 
+			DataOutputStream sendToServer = new DataOutputStream(sock.getOutputStream());
+			//Attaching input stream to socket
+			BufferedReader serverMessage = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			//the system will now prompt the user for input and scan it
+			System.out.println("Waiting for your input: \n");
+			Scanner scan = new Scanner(System.in);
+			String str = scan.next();
+			if(str.equals("QUIT"))
+			{
+				System.out.println("Terminating program");
+				break;
+			}
+			//Sending the user input to server
+			sendToServer.writeBytes(str + '\n');
+			System.out.println("Waiting for the Server to send the length of the user input.");
+			//Receiving the message from server 
+			serverSen = serverMessage.readLine();
+			System.out.println("From server: the last input's length is " +serverSen);
+			scan.close();
+		}
+		sock.close();
+	}
+
 }
